6fb794d2d288ecb22576983fb0326ffed18a6175
Falah <falah.ahmadzubir98@gmail.com>
2024-04-29 12:19:33 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -6,32 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 // -------------------------------------------------- AUTHENTICATION --------------------------------------------------
 
-// UNTUK HANDLE LOGIN
-Route::get('/', [LoginController::class, 'index'])->name('login-page');
-Route::post('/', [LoginController::class, 'store'])->name('submit-login');
-
-// UNTUK HANDLE REGISTER
-Route::get('/register', [RegisterController::class, 'index'])->name('register-page');
-Route::post('/register', [RegisterController::class, 'store'])->name('submit-register');
-
-// UNTUK HANDLE FORGOT PASSWORD
-Route::get('/reset', [PasswordController::class, 'index'])->name('forgot-page');
-Route::post('/reset', [PasswordController::class, 'send'])->name('send-link');
-
-Route::get('/user/{id}/token/{remember_token}', function ($id, $remember_token) {
-    $user = User::find($id);
-    $token = $remember_token;
-    return view('auth.reset-password', compact('user', 'token'));
-});
-
-Route::post('/update_password', [PasswordController::class, 'update'])->name('update-password');
-
-// UNTUK HANDLE LOGOUT
-Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+Route::get('/', [AuthController::class, 'index'])->name('auth.login');
 
 
 
 // -------------------------------------------------- CUSTOMER LIST --------------------------------------------------
 
 Route::get('/customer-list', [CustomerListController::class, 'index'])->name('customer-list');
+Route::get('/customer-profile', [CustomerListController::class, 'customer_profile'])->name('customer-profile');
 
