049bd75fc2b26ef3ef831cac1fd6e6972e34dbac
Falah <falah.ahmadzubir98@gmail.com>
2024-05-07 17:11:41 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/', [AuthController::class, 'login'])->name('login');
-    Route::post('/', [AuthController::class, 'login_post'])->name('login');
+    Route::post('/login', [AuthController::class, 'login_post'])->name('login');
 });
 
 Route::group(['middleware' => 'auth'], function () {
