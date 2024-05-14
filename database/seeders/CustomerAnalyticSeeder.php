b5ab274dc0552c8a49f75d8231494f599e20f43b
Falah <falah.ahmadzubir98@gmail.com>
2024-05-14 16:19:24 +0800

    Update

--- database/seeders/CustomerAnalyticSeeder.php
+++ database/seeders/CustomerAnalyticSeeder.php
@@ -17,26 +17,20 @@ class CustomerAnalyticSeeder extends Seeder
         // Last table row
         $lastProcessedRow = 0;
 
-        // Get data from the analytic customers and customer addresses tables with join
-        $combinedData = DB::connection('ANALYTIC-STG')
+        // Get data from the analytic table starting from the last processed row
+        $customers = DB::connection('ANALYTIC-STG')
             ->table('customers')
-            ->join('customer_addresses', 'customers.id', '=', 'customer_addresses.customer_id')
-            ->select('customers.id', 'customers.customer_name as name', 'customers.customer_tel as phone', 'customer_addresses.street as address', 'customer_addresses.city', 'customer_addresses.postal_code', 'customer_addresses.state_id')
-            ->where('customers.id', '>', $lastProcessedRow)
-            ->take(100)
+            ->where('id', '>', $lastProcessedRow)
+            ->take(1000) // Insert 1000 rows at a time
             ->get();
 
-        // Insert combined data into CRM
-        foreach ($combinedData as $data) {
+        // Insert data into CRM
+        foreach ($customers as $customer) {
             // Insert the customer data into CRM
             DB::connection('CRM-STG')->table('customers')->insert([
-                'id' => $data->id,
-                'name' => $data->name,
-                'phone' => $data->phone,
-                'address' => $data->address,
-                'city' => $data->city,
-                'postcode' => $data->postal_code,
-                'state_id' => $data->state_id,
+                'id' => $customer->id,
+                'name' => $customer->customer_name,
+                'phone' => $customer->customer_tel,
             ]);
         }
     }
