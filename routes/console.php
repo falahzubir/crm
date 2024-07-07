d43bfb0a6488cc0d43d3a732afb49db0c6ad2c7d
Falah <falah.ahmadzubir98@gmail.com>
2024-07-07 15:50:03 +0800

    Create schedule

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,14 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
-// Schedule::command('command:customer-from-analytic')->daily();
+Schedule::command('command:customer-from-analytic')->everyFifteenMinutes();
+Schedule::command('command:tag')->everyFifteenMinutes();
+Schedule::command('command:user')->everyFifteenMinutes();
+Schedule::command('command:role')->everyFifteenMinutes();
+Schedule::command('command:get-username')->everyFifteenMinutes();
+Schedule::command('command:customer-pic')->everyFifteenMinutes();
+Schedule::command('command:customer-tags')->everyFifteenMinutes();
